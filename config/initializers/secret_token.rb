887ccbf84d37970f6a63213652a6a16b88afb146
Angel Villanueva <avillanueva@sinapse.es>
2013-10-31 12:18:13 +0100

    Initial commit after .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+First4::Application.config.secret_key_base = 'b324cd21dd6f57bded666672dc7ae25868d9afb6adc6eee007aaecf7c03ca307950524b1965d3c10d345edace79e0a9523036f0ca93c814967873d10188bc185'
