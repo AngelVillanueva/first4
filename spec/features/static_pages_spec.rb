fd280306b3e361e40e4755d9163547ab727bbfc6
Angel Villanueva <avillanueva@sinapse.es>
2013-11-02 20:33:38 +0100

    Base_title tested via a variable (rspec static pages features)

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) { I18n.t( 'title_row' ) }
 
   describe "Home page" do
 
     it "should work and have a specific content" do
       visit '/static_pages/home'
       page.should have_title(
-        "#{ I18n.t( 'title_row' ) } | #{ I18n.t( 'static_pages.home.title' ) }"
+        "#{ base_title } | #{ I18n.t( 'static_pages.home.title' ) }"
         )
     end
   end
